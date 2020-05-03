6b755626dddb6c676db4bc4dbd2a3f71d9717553
jasonwang <1440719122@qq.com>
2020-05-03 12:21:55 +0800

    ‘个人PC地址修改

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 export default {
 	// serverUrl: 'http://47.113.117.96:3000' // 阿里云服务器地址
-	serverUrl: 'http://localhost/MRC'
+	// serverUrl: 'http://localhost/MRC'
+	serverUrl: 'http://localhost:8090/MRC' // pc
 	// serverUrl: 'http://10.11.1.76:3000' //手机扫码体验请把地址改为局域网ip
 };
